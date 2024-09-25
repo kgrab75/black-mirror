c68c6b05e9636fdab3ae56cad5c8603f43dcf694
kgrab75 <kgrabiner75@gmail.com>
2024-09-25 17:22:04 +0200

    fix: readd noStore

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -3,8 +3,10 @@ import 'regenerator-runtime/runtime';
 import ViewsProvider from '@/app/components/context/ViewsContext';
 import Mirror from '@/app/components/Mirror';
 import { fetchViews } from '@/app/lib/data';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export default async function App() {
+  noStore();
   const views = await fetchViews();
 
   return (
