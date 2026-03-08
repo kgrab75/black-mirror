b5e890d34e5756f800a565a81f3f1e43cd0250b9
kgrab75 <kgrabiner75@gmail.com>
2026-03-08 17:44:06 +0100

    fix: intempestive reload

--- src/app/components/modules/Agenda.tsx
+++ src/app/components/modules/Agenda.tsx
@@ -232,7 +232,6 @@ export default function Agenda(props: AgendaProps) {
     weeksToShow,
     lastEventUpdate,
     hasAccess,
-    showNotification,
     props.options.primaryCalendar?.id,
   ]);
 
